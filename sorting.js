4a1a1af44c576a75ec6b82697f7385e975eb5fa9
Sorin <bulucek.sorin@gmail.com>
2018-05-10 19:18:45 +0000

    custom sortin

---
+++ sorting.js
@@ -0,0 +1,33 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var sortStudents = students.sort(function(a, b) {
+  var nameA = a.name;
+  var nameB = b.name;
+
+  var ageA = a.age;
+  var ageB = b.age;
+
+  if (nameA < nameB) {
+    return -1;
+  } else if (nameA > nameB) {
+    return 1;
+  } else {
+      if (ageA > ageB) {
+        return -1
+      }else if (ageA < ageB){
+        return 1
+      }else {
+        return 0;
+      }
+  }
+})
+
+console.log(sortStudents);
+
+
+//Add CommentCollapse 
